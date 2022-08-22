4c046cf4f0f368f4b4c4f9c7521c5d756f8f0621
muhammadwinanda <muhammadwinanda94@gmail.com>
2022-08-22 08:53:53 +0800

    new file and update animation scroll

---
+++ js/view.js
@@ -0,0 +1,53 @@
+// gsap.registerPlugin(ScrollTrigger)
+
+// gsap.to('.gs_reveal', {
+//   ScrollTrigger: { 
+//   trigger: ".gs_reveal",
+//   toggleAction: "restart none none",
+//   },
+
+//   y: -100,
+//   duration: 3  
+// });
+
+function animateFrom(elem, direction) {
+  direction = direction || 1;
+  var x = 0,
+      y = direction * 100;
+  if(elem.classList.contains("col-left")) {
+    x = -100; 
+    y = 0;
+  } else if (elem.classList.contains("col-right")) {
+    x = 100;
+    y = 0;
+  }
+  elem.style.transform = "translate(" + x + "px, " + y + "px)";
+  elem.style.opacity = "0";
+  gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+    duration: 3, 
+    x: 0,
+    y: 0, 
+    autoAlpha: 1, 
+    ease: "expo", 
+    overwrite: "auto"
+  });
+}
+
+function hide(elem) {
+  gsap.set(elem, {autoAlpha: 0});
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  gsap.registerPlugin(ScrollTrigger);
+  
+  gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
+    hide(elem); // assure that the element is hidden when scrolled into view
+    
+    ScrollTrigger.create({
+      trigger: elem,
+      onEnter: function() { animateFrom(elem) }, 
+      onEnterBack: function() { animateFrom(elem, -1) },
+      onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
+    });
+  });
+});
