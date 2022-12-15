3d46f283146787a99839d021d26dd7b4f04c6053
kokkabanjar <kokkabanjar@gmail.com>
2022-12-16 00:09:56 +0800

    update UI page header, about

--- js/view.js
+++ js/view.js
@@ -1,52 +1,38 @@
-// $.getJSON('js/.json', function (data) {
-//   console.log(data[0]["menu",{[1]}]);
-// });
-
-
-// const ganti = () => {
-//  const kelas = $(".aboutMe") //dengan jquery
-//  for (let i = 0; i < kelas.length; i++) {
-//   kelas[i].innerHTML = "Proses Translate";
-//  }
-// }
-
-// const indo = () => {
-//   changeBahasa();
-// }
-
-// const changeBahasa = () => {
-//   $.getJSON("js/indonesia.json", (data) => {
-//     document.querySelector("a.home").innerHTML = data[0]["menu"]["home"];
-//   });
-// }
+function ulang(){
+  location.reload();
+}
 
 const indo = () => {
-  const tombol = document.querySelector("a.indonesia");
 
   $.getJSON("js/indonesia.json", (data) => {
     const home = document.querySelector("a.home");
     const about = document.querySelector("a.about");
-    const project = document.querySelector("a.project");
+    const skill = document.querySelector("a.skill");
     const contact = document.querySelector("a.contact");
     home.innerHTML = data[0]["menu"]["home"];
     about.innerHTML = data[0]["menu"]["about"];
-    project.innerHTML = data[0]["menu"]["project"];
+    skill.innerHTML = data[0]["menu"]["skill"];
     contact.innerHTML = data[0]["menu"]["contact"];
+    
+    document.querySelector("h2.aboutMe").innerHTML = data[1]["about"]["aboutMe"];
+    document.querySelector("h6.openDialog").innerHTML = data[1]["about"]["dialog"];;
+    document.querySelector("button.clickMe").innerHTML = data[1]["about"]["clickMe"];
+      function clickMe() { 
+        document.querySelector('.intro').innerHTML = data[1]["about"]["intro"]; 
+      }
+      clickMe();
 
-    const aboutMe = document.querySelector("h1.aboutMe");
-    const paragraph = document.querySelector("p.paragraph");
-    aboutMe.innerHTML = data[1]["about"]["aboutMe"];
-    paragraph.innerHTML = data[1]["about"]["paragraph"];
+    document.querySelector("h2.skillMe").innerHTML = data[2]["skill"]["skillMe"];
+    document.querySelector("h5.skill-title").innerHTML = data[2]["skill"]["skillTitle"][0];
+    document.querySelector("h5.skill-title1").innerHTML = data[2]["skill"]["skillTitle"][1];
+    document.querySelector("h5.skill-title2").innerHTML = data[2]["skill"]["skillTitle"][2];
+    document.querySelector("p.skill-text").innerHTML = data[2]["skill"]["skillText"][0];
+    document.querySelector("p.skill-text1").innerHTML = data[2]["skill"]["skillText"][1];
+    document.querySelector("p.skill-text2").innerHTML = data[2]["skill"]["skillText"][2];
+    // document.querySelector("p.skill-time").innerHTML = data[2]["skill"]["skillTime"];
+    // document.querySelector("p.skill-time1").innerHTML = data[2]["skill"]["skillTime"];
 
-    document.querySelector("h1.projectMe").innerHTML = data[2]["project"]["projectMe"];
-    document.querySelector("h5.project-title").innerHTML = data[2]["project"]["projectTitle"][0];
-    document.querySelector("h5.project-title1").innerHTML = data[2]["project"]["projectTitle"][1];
-    document.querySelector("p.project-text").innerHTML = data[2]["project"]["projectText"][0];
-    document.querySelector("p.project-text1").innerHTML = data[2]["project"]["projectText"][1];
-    document.querySelector("p.project-time").innerHTML = data[2]["project"]["projectTime"];
-    document.querySelector("p.project-time1").innerHTML = data[2]["project"]["projectTime"];
-
-    document.querySelector('h1.contactMe').innerHTML = data[3]["contact"]["contactMe"];
+    document.querySelector('h2.contactMe').innerHTML = data[3]["contact"]["contactMe"];
     document.querySelector('label.LName').innerHTML = data[3]["contact"]["LName"];
     document.querySelector('label.LEmail').innerHTML = data[3]["contact"]["LEmail"];
     document.querySelector('label.LMessage').innerHTML = data[3]["contact"]["LMessage"];
@@ -55,33 +41,38 @@ const indo = () => {
 }
 
 const eng = () => {
-  const tombol = document.querySelector("a.england");
-  // tombol.disabled = "true";
 
   $.getJSON("js/english.json", (data) => {
     const home = document.querySelector("a.home");
     const about = document.querySelector("a.about");
-    const project = document.querySelector("a.project");
+    const skill = document.querySelector("a.skill");
     const contact = document.querySelector("a.contact");
     home.innerHTML = data[0]["menu"]["home"];
     about.innerHTML = data[0]["menu"]["about"];
-    project.innerHTML = data[0]["menu"]["project"];
+    skill.innerHTML = data[0]["menu"]["skill"];
     contact.innerHTML = data[0]["menu"]["contact"];
 
-    const aboutMe = document.querySelector("h1.aboutMe");
-    const paragraph = document.querySelector("p.paragraph");
+    const aboutMe = document.querySelector("h2.aboutMe");
     aboutMe.innerHTML = data[1]["about"]["aboutMe"];
-    paragraph.innerHTML = data[1]["about"]["paragraph"];
+    document.querySelector("h6.openDialog").innerHTML = data[1]["about"]["dialog"];
+    document.querySelector("button.clickMe").innerHTML = data[1]["about"]["clickMe"];
+    function clickMe() { 
+      document.querySelector('.intro').innerHTML = data[1]["about"]["intro"]; 
+    }
+    clickMe();
 
-    document.querySelector("h1.projectMe").innerHTML = data[2]["project"]["projectMe"];
-    document.querySelector("h5.project-title").innerHTML = data[2]["project"]["projectTitle"][0];
-    document.querySelector("h5.project-title1").innerHTML = data[2]["project"]["projectTitle"][1];
-    document.querySelector("p.project-text").innerHTML = data[2]["project"]["projectText"][0];
-    document.querySelector("p.project-text1").innerHTML = data[2]["project"]["projectText"][1];
-    document.querySelector("p.project-time").innerHTML = data[2]["project"]["projectTime"];
-    document.querySelector("p.project-time1").innerHTML = data[2]["project"]["projectTime"];
+    document.querySelector("h2.skillMe").innerHTML = data[2]["skill"]["skillMe"];
+    document.querySelector("h5.skill-title").innerHTML = data[2]["skill"]["skillTitle"][0];
+    document.querySelector("h5.skill-title1").innerHTML = data[2]["skill"]["skillTitle"][1];
+    document.querySelector("h5.skill-title2").innerHTML = data[2]["skill"]["skillTitle"][2];
+    document.querySelector("p.skill-text").innerHTML = data[2]["skill"]["skillText"][0];
+    document.querySelector("p.skill-text1").innerHTML = data[2]["skill"]["skillText"][1];
+    document.querySelector("p.skill-text2").innerHTML = data[2]["skill"]["skillText"][2];
+    // document.querySelector("p.skill-time").innerHTML = data[2]["skill"]["skillTime"];
+    // document.querySelector("p.skill-time1").innerHTML = data[2]["skill"]["skillTime"];
 
-    document.querySelector('h1.contactMe').innerHTML = data[3]["contact"]["contactMe"];
+    const test =  document.querySelector('h2.contactMe').innerHTML = data[3]["contact"]["contactMe"];
+    console.log(test);
     document.querySelector('label.LName').innerHTML = data[3]["contact"]["LName"];
     document.querySelector('label.LEmail').innerHTML = data[3]["contact"]["LEmail"];
     document.querySelector('label.LMessage').innerHTML = data[3]["contact"]["LMessage"];
@@ -130,3 +121,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 });
+
+const clickMe = () => {
+  document.querySelector('.intro').classList.toggle('d-none');
+}
