34c648be3105744cbafc4c4f17394ebc2ddd8d65
kokkabanjar <kokkabanjar@gmail.com>
2023-03-19 00:09:10 +0800

    new a page build responsive page and TailwindCSS

--- js/view.js
+++ js/view.js
@@ -3,80 +3,87 @@ function ulang(){
 }
 
 const indo = () => {
-
-  $.getJSON("js/indonesia.json", (data) => {
+  $.getJSON("js/indonesia.json", (indonesia) => {
     const home = document.querySelector("a.home");
     const about = document.querySelector("a.about");
     const skill = document.querySelector("a.skill");
     const contact = document.querySelector("a.contact");
-    home.innerHTML = data[0]["menu"]["home"];
-    about.innerHTML = data[0]["menu"]["about"];
-    skill.innerHTML = data[0]["menu"]["skill"];
-    contact.innerHTML = data[0]["menu"]["contact"];
+    home.innerHTML = indonesia[0]["menu"]["home"];
+    about.innerHTML = indonesia[0]["menu"]["about"];
+    skill.innerHTML = indonesia[0]["menu"]["skill"];
+    contact.innerHTML = indonesia[0]["menu"]["contact"];
     
-    document.querySelector("h2.aboutMe").innerHTML = data[1]["about"]["aboutMe"];
-    document.querySelector("h6.openDialog").innerHTML = data[1]["about"]["dialog"];;
-    document.querySelector("button.clickMe").innerHTML = data[1]["about"]["clickMe"];
+    document.querySelector("h2.aboutMe").innerHTML = indonesia[1]["about"]["aboutMe"];
+    document.querySelector("h6.openDialog").innerHTML = indonesia[1]["about"]["dialog"];;
+    document.querySelector("button.clickMe").innerHTML = indonesia[1]["about"]["clickMe"];
       function clickMe() { 
-        document.querySelector('.intro').innerHTML = data[1]["about"]["intro"]; 
+        document.querySelector('.intro').innerHTML = indonesia[1]["about"]["intro"]; 
       }
       clickMe();
 
-    document.querySelector("h2.skillMe").innerHTML = data[2]["skill"]["skillMe"];
-    document.querySelector("h5.skill-title").innerHTML = data[2]["skill"]["skillTitle"][0];
-    document.querySelector("h5.skill-title1").innerHTML = data[2]["skill"]["skillTitle"][1];
-    document.querySelector("h5.skill-title2").innerHTML = data[2]["skill"]["skillTitle"][2];
-    document.querySelector("p.skill-text").innerHTML = data[2]["skill"]["skillText"][0];
-    document.querySelector("p.skill-text1").innerHTML = data[2]["skill"]["skillText"][1];
-    document.querySelector("p.skill-text2").innerHTML = data[2]["skill"]["skillText"][2];
-    // document.querySelector("p.skill-time").innerHTML = data[2]["skill"]["skillTime"];
-    // document.querySelector("p.skill-time1").innerHTML = data[2]["skill"]["skillTime"];
+    document.querySelector("h2.skillMe").innerHTML = indonesia[2]["skill"]["skillMe"];
+    document.querySelector("h5.skill-title").innerHTML = indonesia[2]["skill"]["skillTitle"][0];
+    document.querySelector("h5.skill-title1").innerHTML = indonesia[2]["skill"]["skillTitle"][1];
+    document.querySelector("h5.skill-title2").innerHTML = indonesia[2]["skill"]["skillTitle"][2];
+    document.querySelector("p.skill-text").innerHTML = indonesia[2]["skill"]["skillText"][0];
+    document.querySelector("p.skill-text1").innerHTML = indonesia[2]["skill"]["skillText"][1];
+    document.querySelector("p.skill-text2").innerHTML = indonesia[2]["skill"]["skillText"][2];
+    // skill a view
+    document.querySelector("td.skill-a-CSS").innerHTML = indonesia[2]["skill"]["skillAView"][0];
+    document.querySelector("td.skill-a-Bootstrap").innerHTML = indonesia[2]["skill"]["skillAView"][1];
+    document.querySelector("td.skill-a-Tailwind").innerHTML = indonesia[2]["skill"]["skillAView"][2];
+    // document.querySelector("p.skill-time").innerHTML = indonesia[2]["skill"]["skillTime"];
+    // document.querySelector("p.skill-time1").innerHTML = indonesia[2]["skill"]["skillTime"];
 
-    document.querySelector('h2.contactMe').innerHTML = data[3]["contact"]["contactMe"];
-    document.querySelector('label.LName').innerHTML = data[3]["contact"]["LName"];
-    document.querySelector('label.LEmail').innerHTML = data[3]["contact"]["LEmail"];
-    document.querySelector('label.LMessage').innerHTML = data[3]["contact"]["LMessage"];
-    document.querySelector('div.my-alert').innerHTML = data[3]["contact"]["my-alert"];
+    document.querySelector('h2.contactMe').innerHTML = indonesia[3]["contact"]["contactMe"];
+    document.querySelector('label.LName').innerHTML = indonesia[3]["contact"]["LName"];
+    document.querySelector('label.LEmail').innerHTML = indonesia[3]["contact"]["LEmail"];
+    document.querySelector('label.LMessage').innerHTML = indonesia[3]["contact"]["LMessage"];
+    document.querySelector('div.my-alert').innerHTML = indonesia[3]["contact"]["my-alert"];
   });
 }
 
 const eng = () => {
-
-  $.getJSON("js/english.json", (data) => {
+  $.getJSON("js/english.json", (english) => {
     const home = document.querySelector("a.home");
     const about = document.querySelector("a.about");
     const skill = document.querySelector("a.skill");
     const contact = document.querySelector("a.contact");
-    home.innerHTML = data[0]["menu"]["home"];
-    about.innerHTML = data[0]["menu"]["about"];
-    skill.innerHTML = data[0]["menu"]["skill"];
-    contact.innerHTML = data[0]["menu"]["contact"];
+    home.innerHTML = english[0]["menu"]["home"];
+    about.innerHTML = english[0]["menu"]["about"];
+    skill.innerHTML = english[0]["menu"]["skill"];
+    contact.innerHTML = english[0]["menu"]["contact"];
 
     const aboutMe = document.querySelector("h2.aboutMe");
-    aboutMe.innerHTML = data[1]["about"]["aboutMe"];
-    document.querySelector("h6.openDialog").innerHTML = data[1]["about"]["dialog"];
-    document.querySelector("button.clickMe").innerHTML = data[1]["about"]["clickMe"];
+    aboutMe.innerHTML = english[1]["about"]["aboutMe"];
+    document.querySelector("h6.openDialog").innerHTML = english[1]["about"]["dialog"];
+    document.querySelector("button.clickMe").innerHTML = english[1]["about"]["clickMe"];
     function clickMe() { 
-      document.querySelector('.intro').innerHTML = data[1]["about"]["intro"]; 
+      document.querySelector('.intro').innerHTML = english[1]["about"]["intro"]; 
     }
     clickMe();
 
-    document.querySelector("h2.skillMe").innerHTML = data[2]["skill"]["skillMe"];
-    document.querySelector("h5.skill-title").innerHTML = data[2]["skill"]["skillTitle"][0];
-    document.querySelector("h5.skill-title1").innerHTML = data[2]["skill"]["skillTitle"][1];
-    document.querySelector("h5.skill-title2").innerHTML = data[2]["skill"]["skillTitle"][2];
-    document.querySelector("p.skill-text").innerHTML = data[2]["skill"]["skillText"][0];
-    document.querySelector("p.skill-text1").innerHTML = data[2]["skill"]["skillText"][1];
-    document.querySelector("p.skill-text2").innerHTML = data[2]["skill"]["skillText"][2];
-    // document.querySelector("p.skill-time").innerHTML = data[2]["skill"]["skillTime"];
-    // document.querySelector("p.skill-time1").innerHTML = data[2]["skill"]["skillTime"];
+    document.querySelector("h2.skillMe").innerHTML = english[2]["skill"]["skillMe"];
+    document.querySelector("h5.skill-title").innerHTML = english[2]["skill"]["skillTitle"][0];
+    document.querySelector("h5.skill-title1").innerHTML = english[2]["skill"]["skillTitle"][1];
+    document.querySelector("h5.skill-title2").innerHTML = english[2]["skill"]["skillTitle"][2];
+    document.querySelector("p.skill-text").innerHTML = english[2]["skill"]["skillText"][0];
+    document.querySelector("p.skill-text1").innerHTML = english[2]["skill"]["skillText"][1];
+    document.querySelector("p.skill-text2").innerHTML = english[2]["skill"]["skillText"][2];
+    // skill a view
+    document.querySelector("td.skill-a-CSS").innerHTML = english[2]["skill"]["skillAView"][0];
+    document.querySelector("td.skill-a-Bootstrap").innerHTML = english[2]["skill"]["skillAView"][1];
+    document.querySelector("td.skill-a-Tailwind").innerHTML = english[2]["skill"]["skillAView"][2];
 
-    const test =  document.querySelector('h2.contactMe').innerHTML = data[3]["contact"]["contactMe"];
+    // document.querySelector("p.skill-time").innerHTML = english[2]["skill"]["skillTime"];
+    // document.querySelector("p.skill-time1").innerHTML = english[2]["skill"]["skillTime"];
+
+    const test =  document.querySelector('h2.contactMe').innerHTML = english[3]["contact"]["contactMe"];
     console.log(test);
-    document.querySelector('label.LName').innerHTML = data[3]["contact"]["LName"];
-    document.querySelector('label.LEmail').innerHTML = data[3]["contact"]["LEmail"];
-    document.querySelector('label.LMessage').innerHTML = data[3]["contact"]["LMessage"];
-    document.querySelector('div.my-alert').innerHTML = data[3]["contact"]["my-alert"];
+    document.querySelector('label.LName').innerHTML = english[3]["contact"]["LName"];
+    document.querySelector('label.LEmail').innerHTML = english[3]["contact"]["LEmail"];
+    document.querySelector('label.LMessage').innerHTML = english[3]["contact"]["LMessage"];
+    document.querySelector('div.my-alert').innerHTML = english[3]["contact"]["my-alert"];
   });
 }
 
